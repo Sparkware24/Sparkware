106f6595e42a68a6fbf0f9596430ef67c65c70ef
shammisk <ssamankumari6@gmail.com>
2024-08-24 19:27:57 +0530

    push pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-function App() {
+import Project from "./pages/Project";
+
+
+
+import Contact from "./pages/Contact";
+
+import WebService from "./sub-pages/services/WebService";
+import MobileService from "./sub-pages/services/MobileService";
+import GraphicService from "./sub-pages/services/GraphicService";
+import SoftwareService from "./sub-pages/services/SoftwareService";
+import EmbeddedService from "./sub-pages/services/EmbeddedService";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex-grow pt-16">
+        <Routes>
+          {/* main pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+          
+
+          {/* service sub pages */}
+          <Route path="/service/web" element={<WebService />} />
+          <Route path="/service/mobile" element={<MobileService />} />
+          <Route path="/service/graphic" element={<GraphicService />} />
+          <Route path="/service/software" element={<SoftwareService />} />
+          <Route path="/service/embedded" element={<EmbeddedService />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
