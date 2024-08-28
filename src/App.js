d733f7c2f3b1b7b0379e62dd02c069a36e22155b
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-08-28 22:51:39 +0530

    add preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -12,8 +12,27 @@ import MobileService from "./sub-pages/services/MobileService";
 import GraphicService from "./sub-pages/services/GraphicService";
 import SoftwareService from "./sub-pages/services/SoftwareService";
 import EmbeddedService from "./sub-pages/services/EmbeddedService";
+import Preloader from "./components/Preloader";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay for images/data to load
+    const loadData = async () => {
+      // Simulate loading data
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
+
+  // Return loader until loading is complete
+  if (loading) {
+    return <Preloader />;
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
@@ -21,7 +40,7 @@ const App = () => {
         <Routes>
           <Route path="/sparkware">
             {/* main pages */}
-            <Route index element={<Home />} /> 
+            <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="project" element={<Project />} />
             <Route path="contact" element={<Contact />} />
