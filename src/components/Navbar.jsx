5223d39ca97a8e1e737d93d682b043e55a81ee7c
shammisk <ssamankumari6@gmail.com>
2024-09-05 10:59:24 +0530

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(null);
   const [mobileDropdownOpen, setMobileDropdownOpen] = useState(null);
-   const location = useLocation();
+  const location = useLocation();
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
@@ -68,189 +68,135 @@ const Navbar = () => {
                 </span>
                 {link.subpages && (
                   <div className="flex items-center h-full">
-                    <ChevronDownIcon className="w-5 h-5 text-white" />
+                    <ChevronDownIcon
+                      className="w-5 h-5 text-white"
+                      aria-hidden="true"
+                    />
                   </div>
                 )}
               </Link>
 
-              {link.subpages &&
-                link.subpages.length < 7 &&
-                dropdownOpen === index && (
-                  <div className="absolute left-0 z-[999] w-48 bg-blue-950 border border-blue-400 rounded shadow-lg group-hover:block">
-                    {link.subpages.map((subpage, subIndex) => (
-                      <Link
-                        key={subIndex}
-                        to={subpage.path}
-                        className={`block px-4 py-2 ${
-                          location.pathname === subpage.path
-                            ? "underline underline-offset-8"
-                            : ""
-                        } hover:underline underline-offset-8`}
-                        onClick={() => {
-                          scrollToTop();
-                          toggleNavbar();
-                        }}
-                      >
-                        <span className="tracking-wide font-semibold hover:underline underline-offset-8 font-mono text-md">
-                          {subpage.name}
-                        </span>
-                      </Link>
-                    ))}
-                  </div>
-                )}
-
-              {link.subpages &&
-                link.subpages.length > 7 &&
-                dropdownOpen === index && (
-                  <div className="flex flex-row absolute left-0 z-[999] w-96 bg-blue-950 border border-blue-400 rounded shadow-lg">
-                    <div className="w-48 group-hover:block border-r border-blue-400">
-                      {link.subpages.slice(0, 9).map((subpage, subIndex) => (
-                        <Link
-                          key={subIndex}
-                          to={subpage.path}
-                          className={`block px-4 py-2 hover:bg-blue-400 ${
-                            location.pathname === subpage.path
-                              ? "underline underline-offset-8"
-                              : ""
-                          }`}
-                          onClick={() => {
-                            scrollToTop();
-                            toggleNavbar();
-                          }}
-                        >
-                          <span className="tracking-wide font-semibold hover:underline underline-offset-8 font-mono  text-md">
-                            {subpage.name}
-                          </span>
-                        </Link>
-                      ))}
-                    </div>
-
-                    <div className="w-48 group-hover:block">
-                      {link.subpages.slice(9).map((subpage, subIndex) => (
-                        <Link
-                          key={subIndex}
-                          to={subpage.path}
-                          className={`block px-4 py-2 hover:bg-blue-400 ${
-                            location.pathname === subpage.path
-                              ? "underline underline-offset-8"
-                              : ""
-                          }`}
-                          onClick={() => {
-                            scrollToTop();
-                            toggleNavbar();
-                          }}
-                        >
-                          <span className="tracking-wide font-semibold hover:underline underline-offset-8 font-mono  text-md">
-                            {subpage.name}
-                          </span>
-                        </Link>
-                      ))}
-                    </div>
-                  </div>
-                )}
+              {link.subpages && dropdownOpen === index && (
+                <div className="absolute left-0 z-[999] w-48 bg-blue-950 border border-blue-400 rounded shadow-lg group-hover:block">
+                  {link.subpages.map((subpage, subIndex) => (
+                    <Link
+                      key={subIndex}
+                      to={subpage.path}
+                      className={`block px-4 py-2 ${
+                        location.pathname === subpage.path
+                          ? "underline underline-offset-8"
+                          : ""
+                      } hover:underline underline-offset-8`}
+                      onClick={() => {
+                        scrollToTop();
+                        toggleNavbar();
+                      }}
+                    >
+                      <span className="tracking-wide font-semibold hover:underline underline-offset-8 font-mono text-md">
+                        {subpage.name}
+                      </span>
+                    </Link>
+                  ))}
+                </div>
+              )}
             </div>
           ))}
         </div>
 
-        {/* WhatsApp Button for Large Screens Only */}
-        <Link
-          to="#"
-          onClick={() => window.open("https://wa.me/94786542882", "_blank")}
-          className="ml-4 hidden xl:block"
-        >
-          <button
-            aria-label="Contact us on WhatsApp"
-            className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded flex items-center"
-          >
-            <PhoneIcon className="w-5 h-5 text-white mr-2" />
-            +9478 654 2882
-          </button>
-        </Link>
-
         {/* Hamburger Icon for Mobile Devices */}
         <div className="xl:hidden">
           <button aria-label="Toggle navigation menu" onClick={toggleNavbar}>
-            <Bars3Icon className="w-6 h-6 text-white" />
+            <Bars3Icon className="w-6 h-6 text-white" aria-hidden="true" />
           </button>
         </div>
       </div>
 
       {/* Side Bar for Mobile Devices */}
       <div
-        className={`fixed top-0 right-0 h-full overflow-y-auto bg-white text-blue-950  transform transition-transform duration-300 ${
+        className={`fixed top-0 right-0 h-full overflow-y-auto bg-white text-blue-950 transform transition-transform duration-300 ${
           isOpen ? "translate-x-0" : "translate-x-full"
         } xl:hidden w-60 sm:w-96 p-4 z-[1000]`}
       >
         <div className="flex justify-between items-center mb-4">
-          <button onClick={toggleNavbar}>
-            <XMarkIcon className="w-6 h-6 text-blue-950 " />
+          <button aria-label="Close sidebar" onClick={toggleNavbar}>
+            <XMarkIcon className="w-6 h-6 text-blue-950 " aria-hidden="true" />
           </button>
         </div>
-        {LINKS.map((link, index) => (
-          <div key={index} className="relative mb-2">
-            <div className="flex flex-row justify-between items-center cursor-pointer px-4 py-2 ">
-              <Link
-                to={link.path}
-                className={`block px-4 py-2 ${
-                  location.pathname === link.path
-                    ? "underline underline-offset-8"
-                    : ""
-                } hover:underline underline-offset-8`}
-                onClick={() => {
-                  scrollToTop();
-                  toggleNavbar();
-                }}
-              >
-                <span className="tracking-wide font-semibold hover:underline underline-offset-8 font-mono  text-md">
-                  {link.name}
-                </span>
-              </Link>
+        <div className="mb-4">
+          {LINKS.map((link, index) => (
+            <div key={index} className="relative mb-2">
+              <div className="flex flex-row justify-between items-center cursor-pointer px-4 py-2">
+                <Link
+                  to={link.path}
+                  className={`block px-4 py-2 ${
+                    location.pathname === link.path
+                      ? "underline underline-offset-8"
+                      : ""
+                  } hover:underline underline-offset-8`}
+                  onClick={() => {
+                    scrollToTop();
+                    toggleNavbar();
+                  }}
+                >
+                  <span className="tracking-wide font-semibold hover:underline underline-offset-8 font-mono text-md">
+                    {link.name}
+                  </span>
+                </Link>
 
-              {link.subpages && (
-                <ChevronDownIcon
-                  onClick={() => toggleMobileDropdown(index)}
-                  className="w-5 h-5 text-blue-950 "
-                />
+                {link.subpages && (
+                  <button
+                    aria-label={`Toggle dropdown for ${link.name}`}
+                    onClick={() => toggleMobileDropdown(index)}
+                  >
+                    <ChevronDownIcon
+                      className="w-5 h-5 text-blue-950 "
+                      aria-hidden="true"
+                    />
+                  </button>
+                )}
+              </div>
+
+              {link.subpages && mobileDropdownOpen === index && (
+                <div className="pl-10">
+                  {link.subpages.map((subpage, subIndex) => (
+                    <Link
+                      key={subIndex}
+                      to={subpage.path}
+                      className={`block px-4 py-2 ${
+                        location.pathname === subpage.path
+                          ? "underline underline-offset-8"
+                          : ""
+                      } hover:underline underline-offset-8`}
+                      onClick={() => {
+                        scrollToTop();
+                        toggleNavbar();
+                      }}
+                    >
+                      <span className="tracking-wide font-semibold hover:underline underline-offset-8 font-mono text-md">
+                        {subpage.name}
+                      </span>
+                    </Link>
+                  ))}
+                </div>
               )}
             </div>
+          ))}
+        </div>
 
-            {link.subpages && mobileDropdownOpen === index && (
-              <div className="pl-10">
-                {link.subpages.map((subpage, subIndex) => (
-                  <Link
-                    key={subIndex}
-                    to={subpage.path}
-                    className={`block px-4 py-2 ${
-                      location.pathname === subpage.path
-                        ? "underline underline-offset-8"
-                        : ""
-                    } hover:underline underline-offset-8`}
-                    onClick={() => {
-                      scrollToTop();
-                      toggleNavbar();
-                    }}
-                  >
-                    <span className="tracking-wide font-semibold hover:underline underline-offset-8 font-mono text-md">
-                      {subpage.name}
-                    </span>
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
-
-        {/* WhatsApp Button in Sidebar for Mobile Devices */}
-        <a
-          href="https://wa.me/94786542882"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Contact us on WhatsApp"
-          className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded flex items-center block mt-4"
+        {/* WhatsApp Button in Sidebar */}
+        <Link
+          to="#"
+          onClick={() => window.open("https://wa.me/94786542882", "_blank")}
+          className="fixed bottom-4 w-full px-4"
         >
-          <PhoneIcon className="w-5 h-5 text-white mr-2" />
-          +9478 654 2882
-        </a>
+          <button
+            aria-label="Contact us on WhatsApp"
+            className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded flex items-center justify-center"
+          >
+            <PhoneIcon className="w-5 h-5 text-white mr-2" aria-hidden="true" />
+            <span>+9478 654 2882</span>
+          </button>
+        </Link>
       </div>
     </nav>
   );
